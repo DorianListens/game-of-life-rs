65954f265bc80de69dc5d8b094d26727a374fca2
Dorian Scheidt <dorian.scheidt@gmail.com>
2017-07-26 03:05:22 -0500

    Implement rules for dead cells

--- src/generator.rs
+++ src/generator.rs
@@ -46,6 +46,38 @@ fn find_neighbours(point: Coordinates) -> Vec<Coordinates> {
     ]
 }
 
+fn process_cell(cell: Cell, neighbours: Vec<CellState>) -> Cell {
+    match cell.cell_state {
+        CellState::Dead => process_dead_cell(cell, neighbours),
+        CellState::Alive => process_living_cell(cell, neighbours),
+    }
+}
+
+fn process_dead_cell(cell: Cell, neighbours: Vec<CellState>) -> Cell {
+    let live_count = neighbours
+        .iter()
+        .filter(|&x| x == &CellState::Alive)
+        .count();
+
+    let new_state = if live_count == 3 {
+        CellState::Alive
+    } else {
+        CellState::Dead
+    };
+
+    Cell {
+        cell_state: new_state,
+        location: cell.location,
+    }
+}
+
+fn process_living_cell(cell: Cell, neighbours: Vec<CellState>) -> Cell {
+    Cell {
+        cell_state: CellState::Dead,
+        location: Coordinates { x: 0, y: 0 },
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,5 +99,74 @@ mod tests {
         let result = find_neighbours(coords);
 
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn a_dead_cell_with_no_neighbours_stays_dead() {
+        let cell = Cell {
+            cell_state: CellState::Dead,
+            location: Coordinates { x: 0, y: 0 },
+        };
+        let neighbours = vec![];
+
+        let processed_cell = process_cell(cell, neighbours);
+
+        assert_eq!(processed_cell.cell_state, CellState::Dead);
+    }
+
+    #[test]
+    fn a_dead_cell_with_dead_neighbours_stays_dead() {
+        use self::CellState::*;
+        let cell = Cell {
+            cell_state: CellState::Dead,
+            location: Coordinates { x: 0, y: 0 },
+        };
+        let neighbours = vec![Dead, Dead, Dead, Dead, Dead, Dead, Dead];
+
+        let processed_cell = process_cell(cell, neighbours);
+
+        assert_eq!(processed_cell.cell_state, CellState::Dead);
+    }
+
+    #[test]
+    fn a_dead_cell_with_less_than_three_live_neighbours_stays_dead() {
+        use self::CellState::*;
+        let cell = Cell {
+            cell_state: CellState::Dead,
+            location: Coordinates { x: 0, y: 0 },
+        };
+        let neighbours = vec![Alive, Alive, Dead, Dead, Dead, Dead, Dead];
+
+        let processed_cell = process_cell(cell, neighbours);
+
+        assert_eq!(processed_cell.cell_state, CellState::Dead);
+    }
+
+    #[test]
+    fn a_dead_cell_with_three_live_neighbours_comes_to_life() {
+        use self::CellState::*;
+        let cell = Cell {
+            cell_state: CellState::Dead,
+            location: Coordinates { x: 0, y: 0 },
+        };
+        let neighbours = vec![Alive, Alive, Alive, Dead, Dead, Dead, Dead];
+
+        let processed_cell = process_cell(cell, neighbours);
+
+        assert_eq!(processed_cell.cell_state, CellState::Alive);
+    }
+
+    #[test]
+    fn a_dead_cell_with_more_than_three_live_neighbours_stays_dead() {
+        use self::CellState::*;
+        let cell = Cell {
+            cell_state: CellState::Dead,
+            location: Coordinates { x: 0, y: 0 },
+        };
+        let neighbours = vec![Alive, Alive, Alive, Alive, Alive, Dead, Dead];
+
+        let processed_cell = process_cell(cell, neighbours);
+
+        assert_eq!(processed_cell.cell_state, CellState::Dead);
     }
 }
