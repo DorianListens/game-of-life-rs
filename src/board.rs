f94419f808f2dfd6fb310ac69c09ee8d82986b98
Dorian Scheidt <dorian.scheidt@gmail.com>
2017-07-25 23:36:12 -0500

    Move more into board mod

--- src/board.rs
+++ src/board.rs
@@ -1,9 +1,27 @@
 use std::collections::HashMap;
-use life::{Cell, CellState, Coordinates};
 
 pub trait Board: Clone {
     fn at(&self, coordiates: Coordinates) -> Option<&Cell>;
 }
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub struct Cell {
+    pub cell_state: CellState,
+    pub location: Coordinates,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub enum CellState {
+    Alive,
+    Dead,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Coordinates {
+    pub x: i32,
+    pub y: i32,
+}
+
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct EmptyBoard {}
@@ -59,5 +77,44 @@ impl SquareBoard {
 impl Board for SquareBoard {
     fn at(&self, coordinates: Coordinates) -> Option<&Cell> {
         self.cells.get(&coordinates)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use board::*;
+
+    #[test]
+    fn a_square_board_all_alive() {
+        let board = SquareBoard::all_alive(5);
+        let x = 0;
+        let y = 0;
+
+        let x2 = 4;
+        let y2 = 4;
+        let cell = board.at(Coordinates { x, y });
+        let cell2 = board.at(Coordinates { x: x2, y: y2 });
+        assert_eq!(
+            cell,
+            Some(&Cell {
+                cell_state: CellState::Alive,
+                location: Coordinates { x, y },
+            })
+        );
+        assert_eq!(
+            cell2,
+            Some(&Cell {
+                cell_state: CellState::Alive,
+                location: Coordinates { x: x2, y: y2 },
+            })
+        );
+    }
+
+    #[test]
+    fn a_square_board_outside_edges_returns_none() {
+        let board = SquareBoard::all_alive(2);
+        let outside_of_board = Coordinates { x: 3, y: 3 };
+        let none_cell = board.at(outside_of_board);
+        assert_eq!(none_cell, None);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,10 @@
 // Any live cell with two or three live neighbours lives on to the next generation.
 // Any live cell with more than three live neighbours dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-//
 
-#![feature(conservative_impl_trait)]
 mod board;
 mod life {
     use board::Board;
-    use std::collections::HashMap;
-    #[derive(Debug, PartialEq, Eq, Clone, Hash)]
-    pub struct Cell {
-        pub cell_state: CellState,
-        pub location: Coordinates,
-    }
-
-    #[derive(Debug, PartialEq, Eq, Clone, Hash)]
-    pub enum CellState {
-        Alive,
-        Dead,
-    }
-
-    #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-    pub struct Coordinates {
-        pub x: i32,
-        pub y: i32,
-    }
 
     pub struct Game<'a, T: Board, U: 'a + Renderer<T>> {
         board: T,
@@ -41,6 +21,11 @@ mod life {
 
         pub fn play(self, generations: u32) -> T {
             self.renderer.render(&self.board);
+
+            for i in 0..generations {
+                self.renderer.render(&self.board);
+            }
+
             self.board
         }
     }
@@ -73,6 +58,16 @@ mod tests {
         assert_eq!(renderer.boards.borrow().first(), Some(&board));
     }
 
+    #[test]
+    fn the_game_renders_the_board_for_each_generation() {
+        let renderer = FakeRenderer::new();
+        let board = EmptyBoard::new();
+        let game = Game::new(board.clone(), &renderer);
+        let _ = game.play(500);
+
+        assert_eq!(renderer.boards.borrow().len(), 501);
+    }
+
     use std::cell::RefCell;
     struct FakeRenderer<T: Board> {
         boards: RefCell<Vec<T>>,
@@ -90,39 +85,5 @@ mod tests {
         fn render(&self, board: &T) {
             self.boards.borrow_mut().push(board.clone());
         }
-    }
-
-    #[test]
-    fn a_square_board_all_alive() {
-        let board = SquareBoard::all_alive(5);
-        let x = 0;
-        let y = 0;
-
-        let x2 = 4;
-        let y2 = 4;
-        let cell = board.at(Coordinates { x, y });
-        let cell2 = board.at(Coordinates { x: x2, y: y2 });
-        assert_eq!(
-            cell,
-            Some(&Cell {
-                cell_state: CellState::Alive,
-                location: Coordinates { x, y },
-            })
-        );
-        assert_eq!(
-            cell2,
-            Some(&Cell {
-                cell_state: CellState::Alive,
-                location: Coordinates { x: x2, y: y2 },
-            })
-        );
-    }
-
-    #[test]
-    fn a_square_board_outside_edges_returns_none() {
-        let board = SquareBoard::all_alive(2);
-        let outside_of_board = Coordinates { x: 3, y: 3 };
-        let none_cell = board.at(outside_of_board);
-        assert_eq!(none_cell, None);
     }
 }
